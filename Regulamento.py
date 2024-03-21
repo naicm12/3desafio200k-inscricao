4c47026530cd042352ca79bc2a4581c5523903f1
naicm12 <163166528+naicm12@users.noreply.github.com>
2024-03-21 17:18:31 -0400

    Delete Regulamento.py

--- Regulamento.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.markdown("#### Regulamento")
-with open('Regulamento.txt','r',encoding='UTF-8') as f:
-    lines = f.readlines()
-    for line in lines:
-        st.write(line)
-
-
